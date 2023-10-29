81e0e6f16cc2f63670e558eed4ffaf1c661431b0
black-adm <mattheuscontato17@gmail.com>
2023-10-29 19:57:09 -0300

    client components and basic for server components

--- src/app/catalog/[...data]/page.tsx
+++ src/app/catalog/[...data]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ShirtsProps {
     params: {
         data: string[]
@@ -5,13 +7,40 @@ interface ShirtsProps {
 }
 
 export default function Product({ params }: ShirtsProps) {
-    const [ shirtId, size, color ] = params.data
+    const [shirtId, color, size] = params.data
+
+    console.log(params)
+
+    function addToCart() {
+        alert(`Produto ${params.data} adicionado ao carrinho.`)
+        console.log(`Você adicionou o produto ${params.data} ao carrinho!`)
+    }
 
     return (
-        <div>
-            <p> Shirt : {shirtId} </p>
-            <p> Size :  {size} </p>
-            <p> Color : {color} </p>
+        <div className="flex flex-col justify-center items-center">
+            <div className="pt-20 pb-8 space-y-4">
+                <p className="text-xl font-bold text-black">
+                    Camiseta -
+                    <strong className="text-lg text-blue-500"> {shirtId} </strong>
+                </p>
+
+                <p className="text-xl font-bold text-black">
+                    Cor -
+                    <strong className="text-lg text-blue-500"> {color} </strong>
+                </p>
+
+                <p className="text-xl font-bold text-black">
+                    Tamanho -
+                    <strong className="text-lg text-blue-500"> {size} </strong>
+                </p>
+            </div>
+
+            <button
+                onClick={addToCart}
+                className="bg-blue-500 px-6 py-2 rounded-md text-white"
+            >
+                Adicionar ao carrinho
+            </button>
         </div>
 
     )
